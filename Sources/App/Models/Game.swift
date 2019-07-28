95e34f50abe320b48a5a63f5cd05ab44775034db
Alexander Trust <atrust@sajonara.de>
2019-07-28 19:49:09 +0200

    Create preliminary Game model and add SQLite config for it.

---
+++ Sources/App/Models/Game.swift
@@ -0,0 +1,27 @@
+//
+//  Game.swift
+//  App
+//
+//  Created by Alexander Trust on 28.07.19.
+//
+
+import Foundation
+import Vapor
+import FluentSQLite
+
+final class Dish: Content {
+    var id: Int?
+    var title: String
+    var pubyear: Int
+    
+    init(title: String, pubyear: Int) {
+        self.title = title
+        self.pubyear = pubyear
+    }
+}
+
+extension Game: SQLiteModel {
+    static let entity: String = "games"
+}
+
+extension Game: Migration {}

a48699db1279a306ec550c1e8d71b461374555cf
Alexander Trust <atrust@sajonara.de>
2019-07-30 18:48:31 +0200

    Create Game Controller. Create route collection and group routes for Game model. Write functions for the Game Controller‘s actions. As of yet there are getAll, getByID, getByGenre, createGame and DeleteGame.

---
+++ Sources/App/Controllers/GameController.swift
@@ -0,0 +1,52 @@
+//
+//  GameController.swift
+//  App
+//
+//  Created by Alexander Trust on 30.07.19.
+//
+
+import Foundation
+import Vapor
+import FluentSQLite
+
+class GameController: RouteCollection {
+    
+    // create routes for the corresponding functions
+    func boot(router: Router) throws {
+        
+        let gamesRoutes = router.grouped("api/games")
+        
+        gamesRoutes.get("/", use: getAll)
+        gamesRoutes.get(Game.parameter, use: getByID)
+        gamesRoutes.get("genres", String.parameter, use: getByGenre)
+        gamesRoutes.post(Game.self, at: "/", use: createGame)
+        gamesRoutes.delete(Game.parameter, use: deleteGame)
+    }
+    
+    // retrieve all games
+    func getAll(req: Request) -> Future<[Game]> {
+        Game.query(on: req).all()
+    }
+    
+    // retrieve a single game
+    func getByID(req: Request) throws -> Future<Game> {
+        return try req.parameters.next(Game.self)
+    }
+    
+    // retrieve games by genre
+    func getByGenre(req: Request) throws -> Future<[Game]> {
+        let genre = try req.parameters.next(String.self)
+        return try Game.query(on: req).filter(\Game.genre == genre).all()
+    }
+    
+    // post a game to the database
+    func createGame(req: Request, game: Game) -> Future<Game> {
+      return game.save(on: req)
+    }
+    
+    // delete a single game
+    func deleteGame(req: Request) throws -> Future<Game> {
+        return try req.parameters.next(Game.self).delete(on: req)
+    }
+    
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,40 +4,6 @@ import FluentSQLite
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "It works" example
-    router.get { req in
-        return "It works!"
-    }
-    
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
-    
-    // Post a game to the database
-    router.post(Game.self, at: "api/game") { req, game -> Future<Game> in
-        return game.save(on: req)
-    }
-    
-    // retrieve games by genre
-    router.get("api/games",String.parameter) { req -> Future<[Game]> in
-        
-        let genre = try req.parameters.next(String.self)
-        return try Game.query(on: req).filter(\Game.genre == genre).all()
-    }
-    
-    // retrieve a list of all games
-    router.get("api/games") { req -> Future<[Game]> in
-        return Game.query(on: req).all()
-    }
-    
-    // retrieve a single game
-    router.get("api/game", Game.parameter) { req -> Future<Game> in
-        return try req.parameters.next(Game.self)
-    }
-    
-    // delete a single game
-    router.delete("api/game", Game.parameter) { req -> Future<Game> in
-        try req.parameters.next(Game.self).delete(on: req)
-    }
+    let gameController = GameController()
+    try router.register(collection: gameController)
 }
